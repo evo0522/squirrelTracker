65d30738722a2596005005a539e0ff03cb2b8908
KarenLiu97 <jl5528@columbia.edu>
2019-11-29 21:13:34 -0500

    all sightings view&urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    #/sightings, A view that lists all squirrel sightings with links to edit and add sightings
+    path('', views.list, name='list'),
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Squirrel
+import datetime as dt
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+#all squirel list 
+def list(request):
+	squirrels = Squirrel.objects.all()
+	context = {
+		'squirrels':squirrels
+	}
+	return render(request,'sightings/all.html',context)
+
