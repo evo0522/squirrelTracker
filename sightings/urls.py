2a9c490fac106e3cb67e500a3fbd9cdfdf4c6b25
KarenLiu97 <jl5528@columbia.edu>
2019-11-29 21:23:16 -0500

    add add view/urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     #/sightings, A view that lists all squirrel sightings with links to edit and add sightings
     path('', views.list, name='list'),
-
+    path('add/', views.add, name='add'),
+    path('add_sighting_from_submission/', views.add_sighting_from_submission, name='add_sighting_from_submission'),
 
 
 
--- sightings/views.py
+++ sightings/views.py
@@ -30,3 +30,73 @@ def single(request, st_id):
 		st_id = st.id
 		st.delete()
 		return HttpResponse('Sighting '+str(st_id)+' has been deleted')
+
+def add(request):
+	return render(request,'sightings/add.html')
+
+def add_sighting_from_submission(request):
+	print("add form is submitted")
+	
+	Latitude = request.POST.get('Latitude')
+	Longitude = request.POST.get('Longitude')
+	Unique_Squirrel_ID = request.POST.get('Unique_Squirrel_ID')
+	Date = request.POST.get('Date') # TODO
+	Age = request.POST.get('Age')
+	Shift = request.POST.get('Shift')
+	Primary_Fur_Color = request.POST.get('Primary_Fur_Color')
+	Location = request.POST.get('Location')
+	Specific_Location = request.POST.get('Specific_Location')
+	Other_Activities = request.POST.get('Other_Activities')
+	# print(request.POST.get('Running'))
+
+	#Boolean
+	Running = True if "true" in request.POST.get('Running').lower() else False
+	Chasing = True if "true" in request.POST.get('Chasing').lower() else False
+	Climbing = True if "true" in request.POST.get('Climbing').lower() else False
+	Eating = True if "true" in request.POST.get('Eating').lower() else False
+	Foraging = True if "true" in request.POST.get('Foraging').lower() else False
+	Kuks = True if "true" in request.POST.get('Kuks').lower() else False
+	Quaas = True if "true" in request.POST.get('Quaas').lower() else False
+	Moans = True if "true" in request.POST.get('Moans').lower() else False
+	Tail_flags = True if "true" in request.POST.get('Tail_flags').lower() else False
+	Tail_twitches = True if "true" in request.POST.get('Tail_twitches').lower() else False
+	Approaches = True if "true" in request.POST.get('Approaches').lower() else False
+	Indifferent = True if "true" in request.POST.get('Indifferent').lower() else False
+	Runs_from = True if "true" in request.POST.get('Runs_from').lower() else False
+	# print(Latitude,Longitude,Age,Shift,Primary_Fur_Color,Location,Specific_Location,Other_Activities)
+	# print(Running,Chasing,Runs_from)
+	squirrel,created = Squirrel.objects.get_or_create(
+                         Longitude = Longitude,
+                         Latitude = Latitude,
+                         Unique_Squirrel_ID = Unique_Squirrel_ID,
+                         Date = dt.datetime.strptime(Date.strip(),'%m%d%Y').date(),
+                         Age = Age,
+                         Shift = Shift,
+                         Primary_Fur_Color = Primary_Fur_Color,
+                         Location = Location,
+                         Specific_Location = Specific_Location,
+                         Other_Activities = Other_Activities,
+                         Running = Running,
+                         Chasing = Chasing,
+                         Climbing = Climbing,
+                         Eating = Eating,
+                         Foraging = Foraging,
+                         Kuks = Kuks,
+                         Quaas = Quaas,
+                         Moans = Moans,
+                         Tail_flags = Tail_flags,
+                         Tail_twitches = Tail_twitches,
+                         Approaches = Approaches,
+                         Indifferent = Indifferent,
+                         Runs_from = Runs_from,
+                         )
+	if created:
+		squirrel.save()
+	print("new sighting addd")
+	squirrels = Squirrel.objects.all()
+	context = {
+		'squirrels':squirrels
+	}
+	return render(request,'sightings/all.html',context)
+
+def update_sighting_from_submission(request):
