71893c5e80c8724568129f34fe69b4b5d54bd9f1
evo0522 <evonnewyh@gmail.com>
2019-11-29 21:17:15 -0500

    add single sighting page

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,4 +5,11 @@ from . import views
 urlpatterns = [
     #/sightings, A view that lists all squirrel sightings with links to edit and add sightings
     path('', views.list, name='list'),
+
+
+
+
+
+    path('<str:st_id>/', views.single, name='squirrel'),
+
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -16,3 +16,17 @@ def list(request):
 	}
 	return render(request,'sightings/all.html',context)
 
+def single(request, st_id):
+	if request.method == 'GET':
+		squirrel = Squirrel.objects.get(id=st_id)
+		context = {
+			'squirrel':squirrel
+		}
+		print(squirrel.id)
+		return render(request,'sightings/sighting.html',context)
+
+	elif request.method == 'DELETE':
+		st = Squirrel.objects.get(id=st_id)
+		st_id = st.id
+		st.delete()
+		return HttpResponse('Sighting '+str(st_id)+' has been deleted')
