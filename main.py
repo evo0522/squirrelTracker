8fcb99171836d61db54ab499f076f7c773251fb1
evo0522 <evonnewyh@gmail.com>
2019-12-08 12:09:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from final_project.wsgi import application
+from squirrelTracker.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
